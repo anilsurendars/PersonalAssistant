f05f2f0ccf92e2fca3bc3c65ae855ee6d436e417
Anilsurendar Sivasankaran <ssubbuanil@gmail.com>
2023-03-16 01:29:14 +0530

    Added automapper

---
+++ PersonalAssistantSolutions/PersonalAssistant.Models/PresentationModels/WebsiteModel.cs
@@ -0,0 +1,16 @@
+﻿namespace PersonalAssistant.Models.PresentationModels;
+
+public class WebsiteModel
+{
+    public int Id { get; set; }
+
+    public string WebsiteName { get; set; } = null!;
+
+    public string Url { get; set; } = null!;
+
+    public string UserName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+}
---
+++ PersonalAssistantSolutions/PersonalAssistant.Models/ServiceConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalAssistant.Models;
+
+public static class ServiceConfiguration
+{
+    public static void RegisterModelComponents(this IServiceCollection serviceCollection, ConfigOption configOption)
+    {
+
+    }
+}
